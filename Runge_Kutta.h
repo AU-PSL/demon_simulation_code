94360a3e183e629de56db552bd51d568213dc9a6
cianci66 <cianci66@mac.com>
2010-12-01 02:41:25 +0000

    Add const qualifiers. Remove unnecessary field from class by refactoring modifyTimeStep to return the current time step. Remove SSE2 intrinsic.

--- Runge_Kutta.h
+++ Runge_Kutta.h
@@ -15,22 +15,21 @@
 class Runge_Kutta
 {
 public:
-	Runge_Kutta(Cloud *myCloud, Force **forces, double timeStep, unsigned int forcesSize, double startTime);	//overloaded constructor
+	Runge_Kutta(Cloud * const myCloud, Force ** const forces, const double timeStep, const unsigned int forcesSize, double startTime);	//overloaded constructor
 
 //public variables:
-	Cloud *cloud;		//pointer to cloud object
-	Force **theForce;	//pointer to Force object
-	unsigned int numForces;
-	double dt;		//current time step
-	double init_dt;		//store initial time step
-	double red_dt;		//store reduced time step
+	Cloud * const cloud;		//pointer to cloud object
+	Force ** const theForce;	//pointer to Force object
+	const unsigned int numForces;
+	const double init_dt;		//store initial time step
+	const double red_dt;        //store reduced time step
 	double currentTime;
 
 //public functions:
 	//Input: double endTime
 	//Preconditions: endTime > 0
 	//Postconditions: Runge-Kutta algorithm complete; position, velocity, time updated.
-	void moveParticles(double endTime);
+	void moveParticles(const double endTime);
 
 private:
 //private functions:
@@ -38,7 +37,7 @@ private:
 	void force2(const double currentTime) const; //rk substep 2
 	void force3(const double currentTime) const; //rk substep 3
 	void force4(const double currentTime) const; //rk substep 4
-	void modifyTimeStep();
+	const double modifyTimeStep() const;
 };
 
 #endif /* RUNGE_KUTTA_H */
