d0fc159b2aa5f1e08d50265c3434151ca6fb8a6f
cianci66@mac.com <cianci66@mac.com@14d88578-3d83-6e1c-c2bc-3e511b5613f6>
2010-12-03 00:16:22 +0000

    Create calculateHeatVal method to provide a single place to calculate the heating value.

--- trunk/TimeVaryingThermalForce.h
+++ trunk/TimeVaryingThermalForce.h
@@ -23,7 +23,7 @@ public:
 	void force2(const double currentTime); //rk substep 2
 	void force3(const double currentTime); //rk substep 3
 	void force4(const double currentTime); //rk substep 4
-
+    
     void writeForce(fitsfile * const file, int * const error) const;
     void readForce(fitsfile * const file, int * const error);
 
@@ -31,6 +31,9 @@ private:
 //private variables
     double heatValScale;
     double heatValOffset;
+    
+//private functions:
+    const double calculateHeatVal(const double currentTime) const;
 };
 
 #endif /* TIMEVARYINGTHERMALFORCE_H */
