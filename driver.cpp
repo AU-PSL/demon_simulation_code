416fa5dec355e6e51eb298c9bd859bd5450685d3
cianci66 <cianci66@mac.com>
2010-12-30 23:59:50 +0000

    Fix handling of command line arguments to error out on unknown options. Remove unneeded break statement.

--- driver.cpp
+++ driver.cpp
@@ -287,7 +287,7 @@ int main (int argc, char * const argv[])
 	unsigned int numForces = 3;
 
 	//process command line flags:
-	for(int i = 0; i < argc; i++)
+	for(int i = 1; i < argc; i++) // argv[0] is the name of the exicutable.
 	{
 		switch(argv[i][1])
 		{
@@ -321,8 +321,7 @@ int main (int argc, char * const argv[])
 				break;
 			case 'h':	//display "h"elp:
 				help();
-				exit(1);
-				break;
+				exit(0);
 			case 'L':	//perform "L"ocalized heating experiment:
 				checkForce('L', 'T', usedForces, ThermalForceLocalizedFlag, ThermalForceFlag);
 				checkForce('L', 'v', usedForces, ThermalForceLocalizedFlag, TimeVaryingThermalForceFlag);
@@ -396,8 +395,10 @@ int main (int argc, char * const argv[])
 				usedForces |= DrivingForceFlag;
 				i = checkOption(argc, argv, i, 'w', "amplitude", &waveAmplitude, "wave shift", &waveShift, "driving constant", &driveConst);
 				break;
-			default:	//default (expressionless)
-				break;
+			default:	// Handle unknown options by issuing error.
+                cout << "Error: Unknown option " << argv[i] << endl;
+                help();
+				exit(0);
 		}
 	}
 
