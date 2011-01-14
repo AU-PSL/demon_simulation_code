f27df44c89dcb804972fd3b3569453d5c1f01f2a
rjef1618 <rjef1618@gmail.com>
2011-01-14 23:14:25 +0000

    Split dimension-specific functions in RectConfinementForce into separate classes.

--- RectConfinementForce.cpp
+++ RectConfinementForce.cpp
@@ -8,94 +8,93 @@
 *===-----------------------------------------------------------------------===*/
 
 #include "RectConfinementForce.h"
+#include "ConfinementForce.h"     //used for 1D
 
+//Constructors:
+RectConfinementForce1D::RectConfinementForce1D(Cloud * const myCloud, double confineConstX)
+: Force(myCloud), confineX(-confineConstX) {}
+RectConfinementForce2D::RectConfinementForce2D(Cloud * const myCloud, double confineConstX, double confineConstY)
+: RectConfinementForce1D(myCloud, confineConstX), confineY(-confineConstY) {}
 RectConfinementForce::RectConfinementForce(Cloud * const myCloud, double confineConstX, double confineConstY, double confineConstZ)
-: Force(myCloud), confineX(-confineConstX), confineY(-confineConstY), confineZ(-confineConstZ) {}
+: RectConfinementForce2D(myCloud, confineConstX, confineConstY), confineZ(-confineConstZ) {}
 
 //1D:
-void RectConfinementForce::force1_1D(const double currentTime)
+void RectConfinementForce1D::force1(const double currentTime)
 {
 	for (unsigned int currentParticle = 0, numParticles = cloud->n; currentParticle < numParticles; currentParticle += 2) 
-		force1D(currentParticle, cloud->getx1_pd(currentParticle));
+		ConfinementForce1D::force(currentParticle, cloud->getx1_pd(currentParticle));
 }
 
-void RectConfinementForce::force2_1D(const double currentTime)
+void RectConfinementForce1D::force2(const double currentTime)
 {
 	for (unsigned int currentParticle = 0, numParticles = cloud->n; currentParticle < numParticles; currentParticle += 2) 
-		force1D(currentParticle, cloud->getx2_pd(currentParticle));
+		ConfinementForce1D::force(currentParticle, cloud->getx2_pd(currentParticle));
 }
 
-void RectConfinementForce::force3_1D(const double currentTime)
+void RectConfinementForce1D::force3(const double currentTime)
 {
 	for (unsigned int currentParticle = 0, numParticles = cloud->n; currentParticle < numParticles; currentParticle += 2)
-		force1D(currentParticle, cloud->getx3_pd(currentParticle));
+		ConfinementForce1D::force(currentParticle, cloud->getx3_pd(currentParticle));
 }
 
-void RectConfinementForce::force4_1D(const double currentTime)
+void RectConfinementForce1D::force4(const double currentTime)
 {
 	for (unsigned int currentParticle = 0, numParticles = cloud->n; currentParticle < numParticles; currentParticle += 2) 
-		force1D(currentParticle, cloud->getx4_pd(currentParticle));
+		ConfinementForce1D::force(currentParticle, cloud->getx4_pd(currentParticle));
 }
 
 //2D:
-void RectConfinementForce::force1_2D(const double currentTime)
+void RectConfinementForce2D::force1(const double currentTime)
 {
 	for (unsigned int currentParticle = 0, numParticles = cloud->n; currentParticle < numParticles; currentParticle += 2) 
-		force2D(currentParticle, cloud->getx1_pd(currentParticle), cloud->gety1_pd(currentParticle));
+		force(currentParticle, cloud->getx1_pd(currentParticle), cloud->gety1_pd(currentParticle));
 }
 
-void RectConfinementForce::force2_2D(const double currentTime)
+void RectConfinementForce2D::force2(const double currentTime)
 {
 	for (unsigned int currentParticle = 0, numParticles = cloud->n; currentParticle < numParticles; currentParticle += 2) 
-		force2D(currentParticle, cloud->getx2_pd(currentParticle), cloud->gety2_pd(currentParticle));
+		force(currentParticle, cloud->getx2_pd(currentParticle), cloud->gety2_pd(currentParticle));
 }
 
-void RectConfinementForce::force3_2D(const double currentTime)
+void RectConfinementForce2D::force3(const double currentTime)
 {
 	for (unsigned int currentParticle = 0, numParticles = cloud->n; currentParticle < numParticles; currentParticle += 2)
-		force2D(currentParticle, cloud->getx3_pd(currentParticle), cloud->gety3_pd(currentParticle));
+		force(currentParticle, cloud->getx3_pd(currentParticle), cloud->gety3_pd(currentParticle));
 }
 
-void RectConfinementForce::force4_2D(const double currentTime)
+void RectConfinementForce2D::force4(const double currentTime)
 {
 	for (unsigned int currentParticle = 0, numParticles = cloud->n; currentParticle < numParticles; currentParticle += 2) 
-		force2D(currentParticle, cloud->getx4_pd(currentParticle), cloud->gety4_pd(currentParticle));
+		force(currentParticle, cloud->getx4_pd(currentParticle), cloud->gety4_pd(currentParticle));
 }
 
 //3D:
-void RectConfinementForce::force1_3D(const double currentTime)
+void RectConfinementForce3D::force1(const double currentTime)
 {
 	for (unsigned int currentParticle = 0, numParticles = cloud->n; currentParticle < numParticles; currentParticle += 2) 
-		force3D(currentParticle, cloud->getx1_pd(currentParticle), cloud->gety1_pd(currentParticle), cloud->getz1_pd(currentParticle));
+		force(currentParticle, cloud->getx1_pd(currentParticle), cloud->gety1_pd(currentParticle), cloud->getz1_pd(currentParticle));
 }
 
-void RectConfinementForce::force2_3D(const double currentTime)
+void RectConfinementForce3D::force2(const double currentTime)
 {
 	for (unsigned int currentParticle = 0, numParticles = cloud->n; currentParticle < numParticles; currentParticle += 2) 
-		force3D(currentParticle, cloud->getx2_pd(currentParticle), cloud->gety2_pd(currentParticle), cloud->getz2_pd(currentParticle));
+		force(currentParticle, cloud->getx2_pd(currentParticle), cloud->gety2_pd(currentParticle), cloud->getz2_pd(currentParticle));
 }
 
-void RectConfinementForce::force3_3D(const double currentTime)
+void RectConfinementForce3D::force3(const double currentTime)
 {
 	for (unsigned int currentParticle = 0, numParticles = cloud->n; currentParticle < numParticles; currentParticle += 2)
-		force3D(currentParticle, cloud->getx3_pd(currentParticle), cloud->gety3_pd(currentParticle), cloud->getz3_pd(currentParticle));
+		force(currentParticle, cloud->getx3_pd(currentParticle), cloud->gety3_pd(currentParticle), cloud->getz3_pd(currentParticle));
 }
 
-void RectConfinementForce::force4_3D(const double currentTime)
+void RectConfinementForce3D::force4(const double currentTime)
 {
 	for (unsigned int currentParticle = 0, numParticles = cloud->n; currentParticle < numParticles; currentParticle += 2) 
-		force3D(currentParticle, cloud->getx4_pd(currentParticle), cloud->gety4_pd(currentParticle), cloud->getz4_pd(currentParticle));
+		force(currentParticle, cloud->getx4_pd(currentParticle), cloud->gety4_pd(currentParticle), cloud->getz4_pd(currentParticle));
 }
 
-//Technically, 1D RectConfinementForce is identical to normal ConfinementForce.
-inline void RectConfinementForce::force1D(const unsigned int currentParticle, const __m128d currentPositionX)
-{
-	double * const pFx = cloud->forceX + currentParticle;
-
-	_mm_store_pd(pFx, _mm_load_pd(pFx) + _mm_set1_pd(confineX)*currentPositionX);
-}
-
-inline void RectConfinementForce::force2D(const unsigned int currentParticle, const __m128d currentPositionX, const __m128d currentPositionY)
+//force methods:
+inline void RectConfinementForce2D::force(const unsigned int currentParticle, const __m128d currentPositionX, const __m128d currentPositionY)
 {
 	double * const pFx = cloud->forceX + currentParticle;
 	double * const pFy = cloud->forceY + currentParticle;
@@ -104,7 +103,7 @@ inline void RectConfinementForce::force2D(const unsigned int currentParticle, co
 	_mm_store_pd(pFy, _mm_load_pd(pFy) + _mm_set1_pd(confineY)*currentPositionY);
 }
 
-inline void RectConfinementForce::force3D(const unsigned int currentParticle, const __m128d currentPositionX, const __m128d currentPositionY, const __m128d currentPositionZ)
+inline void RectConfinementForce3D::force(const unsigned int currentParticle, const __m128d currentPositionX, const __m128d currentPositionY, const __m128d currentPositionZ)
 {
 	double * const pFx = cloud->forceX + currentParticle;
 	double * const pFy = cloud->forceY + currentParticle;
@@ -115,7 +114,8 @@ inline void RectConfinementForce::force3D(const unsigned int currentParticle, co
 	_mm_store_pd(pFz, _mm_load_pd(pFz) + _mm_set1_pd(confineZ)*currentPositionZ);
 }
 
-void RectConfinementForce::writeForce(fitsfile * const file, int * const error, const int dimension) const
+//writeForce:
+void RectConfinementForce1D::writeForce(fitsfile * const file, int * const error) const
 {
 	//move to primary HDU:
 	if(!*error)
@@ -140,27 +140,24 @@ void RectConfinementForce::writeForce(fitsfile * const file, int * const error, 
 	}
 
 	if(!*error)
-	{
-		if(dimension == 1)
-		{
-			//file, key name, value, precision (scientific format), comment
-			fits_write_key_dbl(file, const_cast<char *> ("confineConstX"), confineX, 6, const_cast<char *> ("[N/m] (RectConfinementForce)"), error);
-		}
-		if(dimension == 2)
-		{
-			fits_write_key_dbl(file, const_cast<char *> ("confineConstX"), confineX, 6, const_cast<char *> ("[N/m] (RectConfinementForce)"), error);
-			fits_write_key_dbl(file, const_cast<char *> ("confineConstY"), confineY, 6, const_cast<char *> ("[N/m] (RectConfinementForce)"), error);
-		}
-		if(dimension == 3)
-		{
-			fits_write_key_dbl(file, const_cast<char *> ("confineConstX"), confineX, 6, const_cast<char *> ("[N/m] (RectConfinementForce)"), error);
-			fits_write_key_dbl(file, const_cast<char *> ("confineConstY"), confineY, 6, const_cast<char *> ("[N/m] (RectConfinementForce)"), error);
-			fits_write_key_dbl(file, const_cast<char *> ("confineConstZ"), confineZ, 6, const_cast<char *> ("[N/m] (RectConfinementForce)"), error);
-		}
-	}
+		//file, key name, value, precision (scientific format), comment
+		fits_write_key_dbl(file, const_cast<char *> ("confineConstX"), confineX, 6, const_cast<char *> ("[N/m] (RectConfinementForce)"), error);
 }
 
-void RectConfinementForce::readForce(fitsfile * const file, int * const error, const int dimension)
+void RectConfinementForce2D::writeForce(fitsfile * const  file, int * const error) const
+{
+	RectConfinementForce1D::writeForce(file, error);
+	fits_write_key_dbl(file, const_cast<char *> ("confineConstY"), confineY, 6, const_cast<char *> ("[N/m] (RectConfinementForce)"), error);
+}
+
+void RectConfinementForce2D::writeForce(fitsfile * const  file, int * const error) const
+{
+	RectConfinementForce2D::writeForce(file, error);
+	fits_write_key_dbl(file, const_cast<char *> ("confineConstZ"), confineZ, 6, const_cast<char *> ("[N/m] (RectConfinementForce)"), error);
+}
+
+//readForce:
+void RectConfinementForce1D::readForce(fitsfile * const file, int * const error)
 {
 	//move to primary HDU:
 	if(!*error)
@@ -168,22 +165,18 @@ void RectConfinementForce::readForce(fitsfile * const file, int * const error, c
 		fits_movabs_hdu(file, 1, IMAGE_HDU, error);
 
 	if(!*error)
-	{
-		if(dimension == 1)
-		{
-			//file, key name, value, don't read comment, error
-			fits_read_key_dbl(file, const_cast<char *> ("confineConstX"), &confineX, NULL, error);
-		}
-		if(dimension == 2)
-		{
-			fits_read_key_dbl(file, const_cast<char *> ("confineConstX"), &confineX, NULL, error);
-			fits_read_key_dbl(file, const_cast<char *> ("confineConstY"), &confineY, NULL, error);
-		}
-		if(dimension == 3)
-		{
-			fits_read_key_dbl(file, const_cast<char *> ("confineConstX"), &confineX, NULL, error);
-			fits_read_key_dbl(file, const_cast<char *> ("confineConstY"), &confineY, NULL, error);
-			fits_read_key_dbl(file, const_cast<char *> ("confineConstZ"), &confineZ, NULL, error);
-		}
-	}
+		//file, key name, value, don't read comment, error
+		fits_read_key_dbl(file, const_cast<char *> ("confineConstX"), &confineX, NULL, error);
+}
+
+void RectConfinementForce2D::readForce(fitsfile * const file, int * const error)
+{
+	RectConfinementForce1D::readForce(file, error);
+	fits_read_key_dbl(file, const_cast<char *> ("confineConstY"), &confineY, NULL, error);
+}
+
+void RectConfinementForce3D::readForce(fitsfile * const file, int * const error)
+{
+	RectConfinementForce2D::readForce(file, error);
+	fits_read_key_dbl(file, const_cast<char *> ("confineConstZ"), &confineZ, NULL, error);
 }
