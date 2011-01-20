03e758d52abc1962ddefc9b21c3a4b697999ee4e
cianci66@mac.com <cianci66@mac.com@14d88578-3d83-6e1c-c2bc-3e511b5613f6>
2011-01-20 03:02:23 +0000

    White space and formatting fixes. No functionality change.

--- trunk/PositionVelocityCacheOperator.h
+++ trunk/PositionVelocityCacheOperator.h
@@ -7,20 +7,20 @@
 *
 *===-----------------------------------------------------------------------===*/
 
-#ifndef POSITIONVELOCITYCACHEOPERATOR
-#define POSITIONVELOCITYCACHEOPERATOR
+#ifndef POSITIONVELOCITYCACHEOPERATOR_H
+#define POSITIONVELOCITYCACHEOPERATOR_H
 
 #include "Operator.h"
 
 class PositionVelocityCacheOperator : public Operator {
 public:
-    PositionVelocityCacheOperator(Cloud * const mycloud) : Operator(mycloud) {}
-    ~PositionVelocityCacheOperator() {}
+	PositionVelocityCacheOperator(Cloud * const mycloud) : Operator(mycloud) {}
+	~PositionVelocityCacheOperator() {}
     
-    void operation1(const double currentTime);
-    void operation2(const double currentTime);
-    void operation3(const double currentTime);
-    void operation4(const double currentTime);
+	void operation1(const double currentTime);
+	void operation2(const double currentTime);
+	void operation3(const double currentTime);
+	void operation4(const double currentTime);
 };
 
 #endif // POSITIONVELOCITYCACHEOPERATOR
