aef952940e318986efe8a329a9ba3b19006ccfbc
cianci66 <cianci66@mac.com>
2011-01-20 02:50:31 +0000

    White space and formatting fixes. No functionality change.

--- TimeVaryingDragForce.h
+++ TimeVaryingDragForce.h
@@ -15,25 +15,25 @@
 class TimeVaryingDragForce : public DragForce
 {	
 public:
-	TimeVaryingDragForce(Cloud * const myCloud, const double scale, const double offset);	//overloaded constructor
+	TimeVaryingDragForce(Cloud * const myCloud, const double scale, const double offset);
 	~TimeVaryingDragForce() {} //destructor
 
-//public functions:
-	void force1(const double currentTime); //rk substep 1
-	void force2(const double currentTime); //rk substep 2
-	void force3(const double currentTime); //rk substep 3
-	void force4(const double currentTime); //rk substep 4
+// public functions:
+	void force1(const double currentTime); // rk substep 1
+	void force2(const double currentTime); // rk substep 2
+	void force3(const double currentTime); // rk substep 3
+	void force4(const double currentTime); // rk substep 4
 	
 	void writeForce(fitsfile *file, int *error) const;
 	void readForce(fitsfile *file, int *error);
 
 private:
-//private variables:
-    double scaleConst;	//[s^-2]
-    double offsetConst; //[s^-1]
+// private variables:
+	double scaleConst; // [s^-2]
+	double offsetConst; // [s^-1]
 	
-//private methods:
+// private methods:
 	const double calculateGamma(const double currentTime) const;
 };
 
-#endif /* TIMEVARYINGDRAGFORCE_H */
+#endif // TIMEVARYINGDRAGFORCE_H
