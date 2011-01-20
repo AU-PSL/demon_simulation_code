15a6a5f6b316cf191676cbdcaa79ca3bf488a575
cianci66@mac.com <cianci66@mac.com@14d88578-3d83-6e1c-c2bc-3e511b5613f6>
2011-01-20 02:36:23 +0000

    White space and formatting fixes. No functionality change.

--- trunk/DrivingForce.cpp
+++ trunk/DrivingForce.cpp
@@ -10,8 +10,8 @@
 #include "DrivingForce.h"
 #include <cmath>
 
-const double DrivingForce::waveNum = 2.0*M_PI/0.002; //wavelength = 2mm
-const double DrivingForce::angFreq = 2.0*M_PI*10.0; //10Hz
+const double DrivingForce::waveNum = 2.0*M_PI/0.002; // wavelength = 2mm
+const double DrivingForce::angFreq = 2.0*M_PI*10.0; // 10Hz
 
 DrivingForce::DrivingForce(Cloud * const myCloud, const double drivingConst, const double amp, const double drivingShift)
 : Force(myCloud), amplitude(amp), driveConst(-drivingConst), shift(drivingShift) {}
@@ -47,13 +47,13 @@ void DrivingForce::force4(const double currentTime)
 inline void DrivingForce::force(const unsigned int currentParticle, const __m128d currentTime, const __m128d currentPositionX)
 {	
 	// F = A*sin(k*x - w*t)*exp(-(x + x0)^2/B) is in the paper
-    // NOTE: This is different than the equation listed in the paper. The paper 
-    // is incorrect.
+	// NOTE: This is different than the equation listed in the paper. The paper 
+	// is incorrect.
 	const __m128d distV = currentPositionX - _mm_set1_pd(shift);
 	const __m128d sinArg = _mm_set1_pd(waveNum)*currentPositionX - _mm_set1_pd(angFreq)*currentTime;
 	const __m128d expArg = _mm_set1_pd(-1.0)*distV*distV/_mm_set1_pd(driveConst);
 	
-	//no SIMD trig instructions; break vectors and perform separately:
+	// no SIMD trig instructions; break vectors and perform separately:
 	double sinArgL, sinArgH, expArgL, expArgH;
 	_mm_storel_pd(&sinArgL, sinArg);
 	_mm_storeh_pd(&sinArgH, sinArg);
@@ -66,31 +66,31 @@ inline void DrivingForce::force(const unsigned int currentParticle, const __m128
 
 void DrivingForce::writeForce(fitsfile * const file, int * const error) const
 {
-	//move to primary HDU:
+	// move to primary HDU:
 	if(!*error)
-		//file, # indicating primary HDU, HDU type, error
+		// file, # indicating primary HDU, HDU type, error
  		fits_movabs_hdu(file, 1, IMAGE_HDU, error);
 	
-	//add flag indicating that the driving force is used:
+	// add flag indicating that the driving force is used:
 	if(!*error) 
 	{
 		long forceFlags = 0;
 		fits_read_key_lng(file, const_cast<char *> ("FORCES"), &forceFlags, NULL, error);
 
-		//add DrivingForce bit:
-		forceFlags |= DrivingForceFlag;		//compound bitwise OR
+		// add DrivingForce bit:
+		forceFlags |= DrivingForceFlag; // compound bitwise OR
 
 		if(*error == KEY_NO_EXIST || *error == VALUE_UNDEFINED)
-			*error = 0;			//clear above error.
+			*error = 0; // clear above error.
 
-		//add or update keyword:
+		// add or update keyword:
 		if(!*error) 
 			fits_update_key(file, TLONG, const_cast<char *> ("FORCES"), &forceFlags, const_cast<char *> ("Force configuration."), error);
 	}
 
 	if(!*error)
 	{
-		//file, key name, value, precision (scientific format), comment
+		// file, key name, value, precision (scientific format), comment
 		fits_write_key_dbl(file, const_cast<char *> ("drivingAmplitude"), amplitude, 6, const_cast<char *> ("[N] (DrivingForce)"), error);
 		fits_write_key_dbl(file, const_cast<char *> ("drivingConst"), driveConst, 6, const_cast<char *> ("[m^2] (DrivingForce)"), error);
 		fits_write_key_dbl(file, const_cast<char *> ("drivingShift"), shift, 6, const_cast<char *> ("[m] (DrivingForce)"), error);
@@ -99,14 +99,14 @@ void DrivingForce::writeForce(fitsfile * const file, int * const error) const
 
 void DrivingForce::readForce(fitsfile * const file, int * const error)
 {
-	//move to primary HDU:
+	// move to primary HDU:
 	if(!*error)
-		//file, # indicating primary HDU, HDU type, error
+		// file, # indicating primary HDU, HDU type, error
  		fits_movabs_hdu(file, 1, IMAGE_HDU, error);
 	
 	if(!*error)
 	{
-		//file, key name, value, don't read comment, error
+		// file, key name, value, don't read comment, error
 		fits_read_key_dbl(file, const_cast<char *> ("drivingAmplitude"), &amplitude, NULL, error);
 		fits_read_key_dbl(file, const_cast<char *> ("drivingConst"), &driveConst, NULL, error);
 		fits_read_key_dbl(file, const_cast<char *> ("drivingShift"), &shift, NULL, error);
