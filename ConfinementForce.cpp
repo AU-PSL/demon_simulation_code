edd7bd9165a20afd52a03636fea1b832fcfb273d
cianci66 <cianci66@mac.com>
2011-01-20 02:39:16 +0000

    White space and formatting fixes. No functionality change.

--- ConfinementForce.cpp
+++ ConfinementForce.cpp
@@ -47,41 +47,41 @@ inline void ConfinementForce::force(const unsigned int currentParticle, const __
 
 void ConfinementForce::writeForce(fitsfile * const file, int * const error) const
 {
-	//move to primary HDU:
+	// move to primary HDU:
 	if(!*error)
-		//file, # indicating primary HDU, HDU type, error
+		// file, # indicating primary HDU, HDU type, error
  		fits_movabs_hdu(file, 1, IMAGE_HDU, error);
 	
-	//add flag indicating that the confinement force is used:
+	// add flag indicating that the confinement force is used:
 	if (!*error) 
 	{
 		long forceFlags = 0;
 		fits_read_key_lng(file, const_cast<char *> ("FORCES"), &forceFlags, NULL, error);
 
-		//add ConfinementForce bit:
-		forceFlags |= ConfinementForceFlag;	//compound bitwise OR
+		// add ConfinementForce bit:
+		forceFlags |= ConfinementForceFlag;	// compound bitwise OR
 
 		if(*error == KEY_NO_EXIST || *error == VALUE_UNDEFINED)
-			*error = 0;			//clear above error
+			*error = 0; // clear above error
 
-		//add or update keyword:
+		// add or update keyword:
 		if(!*error) 
 			fits_update_key(file, TLONG, const_cast<char *> ("FORCES"), &forceFlags, const_cast<char *> ("Force configuration."), error);
 	}
 
 	if(!*error)
-		//file, key name, value, precision (scientific format), comment
+		// file, key name, value, precision (scientific format), comment
 		fits_write_key_dbl(file, const_cast<char *> ("confineConst"), confine, 6, const_cast<char *> ("[N/m] (ConfinementForce)"), error);
 }
 
 void ConfinementForce::readForce(fitsfile * const file, int * const error)
 {
-	//move to primary HDU:
+	// move to primary HDU:
 	if(!*error)
-		//file, # indicating primary HDU, HDU type, error
+		// file, # indicating primary HDU, HDU type, error
  		fits_movabs_hdu(file, 1, IMAGE_HDU, error);
 
 	if(!*error)
-		//file, key name, value, don't read comment, error
+		// file, key name, value, don't read comment, error
 		fits_read_key_dbl(file, const_cast<char *> ("confineConst"), &confine, NULL, error);
 }
