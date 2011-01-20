ef383b8e9f56144b6c4a3b6533ebb15c837c8764
cianci66@mac.com <cianci66@mac.com@14d88578-3d83-6e1c-c2bc-3e511b5613f6>
2011-01-20 02:21:20 +0000

    White Space and comment formatting changes no functionality change.

--- trunk/Force.h
+++ trunk/Force.h
@@ -14,16 +14,16 @@
 
 // Binary assignments for the bit-packed FORCES keyword in Fits file:
 enum ForceFlag {
-	ConfinementForceFlag = 1,         //0000000001
-	DragForceFlag = 2,                //0000000010
-	ShieldedCoulombForceFlag = 4,     //0000000100
-	RectConfinementForceFlag = 8,     //0000001000
-	ThermalForceFlag = 16,            //0000010000
-	ThermalForceLocalizedFlag = 32,   //0000100000
-	DrivingForceFlag = 64,            //0001000000
-	RotationalForceFlag = 128,        //0010000000
-	TimeVaryingDragForceFlag = 256,   //0100000000
-	TimeVaryingThermalForceFlag = 512 //1000000000
+	ConfinementForceFlag = 1,         // 0000000001
+	DragForceFlag = 2,                // 0000000010
+	ShieldedCoulombForceFlag = 4,     // 0000000100
+	RectConfinementForceFlag = 8,     // 0000001000
+	ThermalForceFlag = 16,            // 0000010000
+	ThermalForceLocalizedFlag = 32,   // 0000100000
+	DrivingForceFlag = 64,            // 0001000000
+	RotationalForceFlag = 128,        // 0010000000
+	TimeVaryingDragForceFlag = 256,   // 0100000000
+	TimeVaryingThermalForceFlag = 512 // 1000000000
 };
 
 class Force
@@ -32,16 +32,16 @@ public:
 	Cloud * const cloud;
 	
 	Force(Cloud * const myCloud) : cloud(myCloud) {} 
-	virtual ~Force() {} //implementation of virtual destructor
+	virtual ~Force() {} // implementation of virtual destructor
 
-	//Note: currentTime parameter necessary for DrivingForce, unused in others
-	virtual void force1(const double currentTime)=0; //rk substep 1
-	virtual void force2(const double currentTime)=0; //rk substep 2
-	virtual void force3(const double currentTime)=0; //rk substep 3
-	virtual void force4(const double currentTime)=0; //rk substep 4
+	// Note: currentTime parameter necessary for DrivingForce, unused in others
+	virtual void force1(const double currentTime)=0; // rk substep 1
+	virtual void force2(const double currentTime)=0; // rk substep 2
+	virtual void force3(const double currentTime)=0; // rk substep 3
+	virtual void force4(const double currentTime)=0; // rk substep 4
 
-	virtual void writeForce(fitsfile * const file, int * const error) const=0;	//output force information to file
-	virtual void readForce(fitsfile * const file, int * const error)=0;	//read force information from file
+	virtual void writeForce(fitsfile * const file, int * const error) const=0;	// output force information to file
+	virtual void readForce(fitsfile * const file, int * const error)=0;	// read force information from file
 };
 
-#endif /* FORCE_H */
+#endif // FORCE_H
