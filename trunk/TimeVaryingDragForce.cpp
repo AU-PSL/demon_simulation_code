f591af1d6e6739b1c50f858f73914621be49b415
cianci66@mac.com <cianci66@mac.com@14d88578-3d83-6e1c-c2bc-3e511b5613f6>
2011-01-20 02:50:31 +0000

    White space and formatting fixes. No functionality change.

--- trunk/TimeVaryingDragForce.cpp
+++ trunk/TimeVaryingDragForce.cpp
@@ -43,31 +43,31 @@ inline const double TimeVaryingDragForce::calculateGamma(const double currentTim
 
 void TimeVaryingDragForce::writeForce(fitsfile * const file, int * const error) const
 {
-	//move to primary HDU:
+	// move to primary HDU:
 	if(!*error)
-		//file, # indicating primary HDU, HDU type, error
+		// file, # indicating primary HDU, HDU type, error
  		fits_movabs_hdu(file, 1, IMAGE_HDU, error);
 	
-	//add flag indicating that the time varying drag force is used:
+	// add flag indicating that the time varying drag force is used:
 	if (!*error) 
 	{
 		long forceFlags = 0;
 		fits_read_key_lng(file, const_cast<char *> ("FORCES"), &forceFlags, NULL, error);
 
-		//add TimeVaryingDragForce bit:
+		// add TimeVaryingDragForce bit:
 		forceFlags |= TimeVaryingDragForceFlag;
 
 		if(*error == KEY_NO_EXIST || *error == VALUE_UNDEFINED)
-			*error = 0;			//clear above error.
+			*error = 0; // clear above error.
 
-		//add or update keyword:
+		// add or update keyword:
 		if (!*error) 
 			fits_update_key(file, TLONG, const_cast<char *> ("FORCES"), &forceFlags, const_cast<char *> ("Force configureation."), error);
 	}
 		
 	if(!*error)
 	{
-		//file, key name, value, precision (scientific format), comment
+		// file, key name, value, precision (scientific format), comment
 		fits_write_key_dbl(file, const_cast<char *> ("TVDragScaleConst"), scaleConst, 6, const_cast<char *> ("[s^-2] (TimeVaryingDragForce)"), error);
 		fits_write_key_dbl(file, const_cast<char *> ("TVDragOffsetConst"), offsetConst, 6, const_cast<char *> ("[s^-1] (TimeVaryingDragForce)"), error);
 	}
@@ -75,14 +75,14 @@ void TimeVaryingDragForce::writeForce(fitsfile * const file, int * const error) 
 
 void TimeVaryingDragForce::readForce(fitsfile * const file, int * const error)
 {
-	//move to primary HDU:
+	// move to primary HDU:
 	if(!*error)
-		//file, # indicating primary HDU, HDU type, error
+		// file, # indicating primary HDU, HDU type, error
  		fits_movabs_hdu(file, 1, IMAGE_HDU, error);
 	
 	if(!*error)
 	{
-		//file, key name, value, don't read comment, error
+		// file, key name, value, don't read comment, error
 		fits_read_key_dbl(file, const_cast<char *> ("TVDragScaleConst"), &scaleConst, NULL, error);
 		fits_read_key_dbl(file, const_cast<char *> ("TVDragOffsetConst"), &offsetConst, NULL, error);
 	}
