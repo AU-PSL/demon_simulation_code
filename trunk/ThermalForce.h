261fe5400cfaa4e44ed44f0d513ffebba81b6301
cianci66@mac.com <cianci66@mac.com@14d88578-3d83-6e1c-c2bc-3e511b5613f6>
2011-01-20 02:52:58 +0000

    White space and formatting fixes. No functionality change.

--- trunk/ThermalForce.h
+++ trunk/ThermalForce.h
@@ -11,34 +11,34 @@
 #define THERMALFORCE_H
 
 #include "Force.h"
-#include "mtrand.h"	//MT header
+#include "mtrand.h"	// MT header
 
 class ThermalForce : public Force
 {	
 public:
-	ThermalForce(Cloud * const myCloud, const double redFactor);	//overloaded constructor
-	~ThermalForce() {} //destructor
+	ThermalForce(Cloud * const myCloud, const double redFactor);
+	~ThermalForce() {} // destructor
 
-//public functions:
-	//Note: currentTime parameter is necessary (due to parent class) but unused
-	virtual void force1(const double currentTime); //rk substep 1
-	virtual void force2(const double currentTime); //rk substep 2
-	virtual void force3(const double currentTime); //rk substep 3
-	virtual void force4(const double currentTime); //rk substep 4
+// public functions:
+	// Note: currentTime parameter is necessary (due to parent class) but unused
+	virtual void force1(const double currentTime); // rk substep 1
+	virtual void force2(const double currentTime); // rk substep 2
+	virtual void force3(const double currentTime); // rk substep 3
+	virtual void force4(const double currentTime); // rk substep 4
 
 	virtual void writeForce(fitsfile * const file, int * const error) const;
 	virtual void readForce(fitsfile * const file, int * const error);
 
 private:
-//private variables:
-    MTRand mt;
+// private variables:
+	MTRand mt;
 
-//private functions:
+// private functions:
 	void force(const unsigned int currentParticle);
 
 protected:
-//protected variables:
+// protected variables:
 	double heatVal;
 };
 
-#endif /* THERMALFORCE_H */
+#endif // THERMALFORCE_H
