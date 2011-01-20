c6319773cf4ed97eed6aae9febbe5805ee24cb2f
cianci66 <cianci66@mac.com>
2011-01-20 02:32:44 +0000

    White space and formatting fixes. No functionality change.

--- ShieldedCoulombForce.h
+++ ShieldedCoulombForce.h
@@ -19,24 +19,24 @@ public:
 	ShieldedCoulombForce(Cloud * const myCloud, const double shieldingConstant);	//overloaded constructor
 	~ShieldedCoulombForce() {} //destructor
 
-//public functions:
-	//Note: currentTime parameter is necessary (due to parent class) but unused
+// public functions:
+	// Note: currentTime parameter is necessary (due to parent class) but unused
 	void force1(const double currentTime); //rk substep 1
 	void force2(const double currentTime); //rk substep 2
 	void force3(const double currentTime); //rk substep 3
 	void force4(const double currentTime); //rk substep 4
 
-    void writeForce(fitsfile * const file, int * const error) const;
-    void readForce(fitsfile * const file, int * const error);
+	void writeForce(fitsfile * const file, int * const error) const;
+	void readForce(fitsfile * const file, int * const error);
 
 private:
-//public variables:
-    double shielding;
+// public variables:
+	double shielding;
 
-//private functions:
+// private functions:
 	void force(const unsigned int currentParticle, const unsigned int iParticle, const double displacementX, const double displacementY);
-    void force(const unsigned int currentParticle, const unsigned int iParticle, const __m128d displacementX, const __m128d displacementY);
-    void forcer(const unsigned int currentParticle, const unsigned int iParticle, const __m128d displacementX, const __m128d displacementY);
+	void force(const unsigned int currentParticle, const unsigned int iParticle, const __m128d displacementX, const __m128d displacementY);
+	void forcer(const unsigned int currentParticle, const unsigned int iParticle, const __m128d displacementX, const __m128d displacementY);
 };
 
-#endif /* SHIELDEDCOULOMBFORCE_H */
+#endif // SHIELDEDCOULOMBFORCE_H
