bafd5c10fc3a5f55209fc851bed9523a1571a7bd
cianci66@mac.com <cianci66@mac.com@14d88578-3d83-6e1c-c2bc-3e511b5613f6>
2011-01-27 18:26:11 +0000

    Parallelize for loops in Runga-Kutta.

--- branches/parallel/gcd/Runge_Kutta.cpp
+++ branches/parallel/gcd/Runge_Kutta.cpp
@@ -37,8 +37,8 @@ void Runge_Kutta::moveParticles(const double endTime)
         
 		operate1(currentTime);
 		force1(currentTime); // compute net force1
-		for (cloud_index i = 0, numParticles = cloud->n; i < numParticles; i += 2) // calculate k1 and l1 for entire cloud
-		{
+		dispatch_apply(cloud->n/2, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^(size_t i) {
+			i *= 2;
 			const __m128d vmass = _mm_load_pd(cloud->mass + i); // load ith and (i+1)th mass into vector
 
 			// assign force pointers for stylistic purposes:
@@ -54,12 +54,12 @@ void Runge_Kutta::moveParticles(const double endTime)
 			// reset forces to zero:
 			_mm_store_pd(pFx, _mm_setzero_pd());
 			_mm_store_pd(pFy, _mm_setzero_pd());
-		}
+		});
         
 		operate2(currentTime + dt/2.0);
 		force2(currentTime + dt/2.0); // compute net force2
-		for (cloud_index i = 0, numParticles = cloud->n; i < numParticles; i += 2) // calculate k2 and l2 for entire cloud
-		{
+		dispatch_apply(cloud->n/2, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^(size_t i) {
+			i *= 2;
 			const __m128d vmass = _mm_load_pd(cloud->mass + i); // load ith and (i+1)th mass
 
 			// assign force pointers:
@@ -75,12 +75,12 @@ void Runge_Kutta::moveParticles(const double endTime)
 			// reset forces to zero:
 			_mm_store_pd(pFx, _mm_setzero_pd());
 			_mm_store_pd(pFy, _mm_setzero_pd());
-		}
+		});
 
 		operate3(currentTime + dt/2.0);
 		force3(currentTime + dt/2.0); // compute net force3
-		for (cloud_index i = 0, numParticles = cloud->n; i < numParticles; i += 2) // calculate k3 and l3 for entire cloud
-		{
+		dispatch_apply(cloud->n/2, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^(size_t i) {
+			i *= 2;
 			const __m128d vmass = _mm_load_pd(cloud->mass + i); // load ith and (i+1)th mass
 
 			// assign force pointers:
@@ -96,12 +96,12 @@ void Runge_Kutta::moveParticles(const double endTime)
 			// reset forces to zero:
 			_mm_store_pd(pFx, _mm_setzero_pd());
 			_mm_store_pd(pFy, _mm_setzero_pd());
-		}
+		});
         
 		operate4(currentTime + dt/2.0);
 		force4(currentTime + dt); // compute net force4
-		for (cloud_index i = 0, numParticles = cloud->n; i < numParticles; i += 2) // calculate k4 and l4 for entire cloud
-		{
+		dispatch_apply(cloud->n/2, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^(size_t i) {
+			i *= 2;
 			const __m128d vmass = _mm_load_pd(cloud->mass + i); // load ith and (i+1)th mass
 
 			// assign force pointers:
@@ -116,10 +116,10 @@ void Runge_Kutta::moveParticles(const double endTime)
 			// reset forces to zero:
 			_mm_store_pd(pFx, _mm_setzero_pd());
 			_mm_store_pd(pFy, _mm_setzero_pd());
-		}
+		});
 
-		for (cloud_index i = 0, numParticles = cloud->n; i < numParticles; i += 2) // calculate next position and next velocity for entire cloud
-		{
+		dispatch_apply(cloud->n/2, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^(size_t i) {
+			i *= 2;
 			// load ith and (i+1)th k's into vectors:
 			const __m128d vk1 = _mm_load_pd(cloud->k1 + i);
 			const __m128d vk2 = _mm_load_pd(cloud->k2 + i);
@@ -155,7 +155,7 @@ void Runge_Kutta::moveParticles(const double endTime)
 			_mm_store_pd(px, _mm_load_pd(px) + (vl1 + v2*(vl2 + vl3) + vl4)/v6);
 			_mm_store_pd(pVy, _mm_load_pd(pVy) + (vm1 + v2*(vm2 + vm3) + vm4)/v6);
 			_mm_store_pd(py, _mm_load_pd(py) + (vn1 + v2*(vn2 + vn3) + vn4)/v6);
-		}
+		});
 
 		currentTime += dt;
 	}
