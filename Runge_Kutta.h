fdad6dde824b6e5dd65530f0f2aaba33f13d399a
cianci66 <cianci66@mac.com>
2011-02-24 04:53:06 +0000

    Vector comparison code out into it's own method. This simplifies the time step modification.

--- Runge_Kutta.h
+++ Runge_Kutta.h
@@ -48,7 +48,9 @@ private:
 	void force2(const double currentTime) const; // rk substep 2
 	void force3(const double currentTime) const; // rk substep 3
 	void force4(const double currentTime) const; // rk substep 4
+
 	const double modifyTimeStep(cloud_index outerIndex, cloud_index innerIndex, const double currentDist, const double currentTimeStep) const;
+	static bool isLessThanOrEqualTo(const __m128d a, const __m128d b);
 };
 
 #endif // RUNGE_KUTTA_H
