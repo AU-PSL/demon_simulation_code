ae0236b2ec6589d15f8f8a4cbf124cab24c67cf1
cianci66@mac.com <cianci66@mac.com@14d88578-3d83-6e1c-c2bc-3e511b5613f6>
2011-06-09 23:52:43 +0000

    Tag version 1.5
    Changes:
    Updated and cleaned up driver.

---
+++ tags/DEMON-1.5/RectConfinementForce.h
@@ -0,0 +1,42 @@
+/*===- RectConfinementForce.h - libSimulation -=================================
+*
+*                                  DEMON
+* 
+* This file is distributed under the BSD Open Source License. See LICENSE.TXT  
+* for details. 
+*
+*===-----------------------------------------------------------------------===*/
+
+#ifndef RECTCONFINEMENTFORCE_H
+#define RECTCONFINEMENTFORCE_H
+
+#include "Force.h"
+#include "VectorCompatibility.h"
+
+class RectConfinementForce : public Force
+{	
+public:
+	RectConfinementForce(Cloud * const myCloud, double confineConstX, double confineConstY);
+	// IMPORTANT: In the above constructor, confineConst_'s must be positive!
+	~RectConfinementForce() {} // destructor
+
+// public functions:
+	// Note: currentTime parameter is necessary (due to parent class) but unused
+	void force1(const double currentTime); // rk substep 1
+	void force2(const double currentTime); // rk substep 2
+	void force3(const double currentTime); // rk substep 3
+	void force4(const double currentTime); // rk substep 4
+
+	void writeForce(fitsfile * const file, int * const error) const;
+	void readForce(fitsfile * const file, int * const error);
+
+private:
+// private variables:
+	double confineX;
+	double confineY;
+
+// private functions:
+	void force(const cloud_index currentParticle, const __m128d currentPositionX, const __m128d currentPositionY);
+};
+
+#endif // RECTCONFINEMENTFORCE_H
---
+++ tags/DEMON-1.5/VectorCompatibility.h
@@ -0,0 +1,37 @@
+/*===- VectorCompatibility.h - libSimulation -==================================
+*
+*                                  DEMON
+*
+* This file is distributed under the BSD Open Source License. See LICENSE.TXT 
+* for details.
+*
+*===-----------------------------------------------------------------------===*/
+
+#ifndef VECTORCOMPATIBILITY_H
+#define VECTORCOMPATIBILITY_H
+
+#include <smmintrin.h>
+
+#if !defined(__GNUC__) && !defined(__clang__)
+__m128d operator+(const __m128d &a, const __m128d &b) {
+	return _mm_add_pd(a, b);
+}
+
+__m128d operator-(const __m128d &a, const __m128d &b) {
+	return _mm_sub_pd(a, b);
+}
+
+__m128d operator*(const __m128d &a, const __m128d &b) {
+	return _mm_mul_pd(a, b);
+}
+
+__m128d operator/(const __m128d &a, const __m128d &b) {
+	return _mm_div_pd(a, b);
+}
+
+__m128d operator&&(const __m128d &a, const __m128d &b) {
+	return _mm_and_pd(a, b);
+}
+#endif
+
+#endif // VECTORCOMPATIBILITY_H
