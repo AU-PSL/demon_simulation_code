cfd725ebc87d0af74060d173e4893beabb6fa4b1
rjef1618@gmail.com <rjef1618@gmail.com@14d88578-3d83-6e1c-c2bc-3e511b5613f6>
2011-06-17 19:01:32 +0000

    Removed accumulating charge bug. Testing code remains in place due to problems with qConst1, qConst2.

--- trunk/Runge_Kutta.cpp
+++ trunk/Runge_Kutta.cpp
@@ -72,8 +72,9 @@ void Runge_Kutta::moveParticles(const double endTime)
 			_mm_store_pd(cloud->l1 + i, vdt*cloud->getVx1_pd(i)); // positionX tidbit
 			_mm_store_pd(cloud->m1 + i, vdt*_mm_load_pd(pFy)/vmass); // velocityY tidbit
 			_mm_store_pd(cloud->n1 + i, vdt*cloud->getVy1_pd(i)); // positionY tidbit
-			_mm_store_pd(cloud->q1 + i, -vdt*(qConst1*_mm_load_pd(cloud->charge + i) +
-				qConst2*partRadius*_mm_load_pd(cloud->phi + i)));
+			//_mm_store_pd(cloud->q1 + i, -vdt*(qConst1*_mm_load_pd(cloud->charge + i) +
+			//	qConst2*partRadius*_mm_load_pd(cloud->phi + i)));
+			_mm_store_pd(cloud->q1 + i, _mm_load_pd(cloud->charge + i));
 				// see "Instability of Dust Acoustic Waves in an Accelerating Dusy Plasma"
 				// P. K. Shukla, M. Salimullah, G. E. Morfill
 				// Physica Scripta, Vol. 67, 354-356, 2003
@@ -105,8 +106,9 @@ void Runge_Kutta::moveParticles(const double endTime)
 			_mm_store_pd(cloud->l2 + i, vdt*cloud->getVx2_pd(i)); // positionX tidbit
 			_mm_store_pd(cloud->m2 + i, vdt*_mm_load_pd(pFy)/vmass); // velocityY tidbit
 			_mm_store_pd(cloud->n2 + i, vdt*cloud->getVy2_pd(i)); // positionY tidbit
-			_mm_store_pd(cloud->q2 + i, -vdt*(qConst1*cloud->getq1_pd(i) +
-				qConst2*partRadius*_mm_load_pd(cloud->phi + i)));
+			//_mm_store_pd(cloud->q2 + i, -vdt*(qConst1*cloud->getq1_pd(i) +
+			//	qConst2*partRadius*_mm_load_pd(cloud->phi + i)));
+			_mm_store_pd(cloud->q2 + i, _mm_load_pd(cloud->charge + i));
 
 			// reset to zero:
 			_mm_store_pd(pFx, _mm_setzero_pd());
@@ -134,8 +136,9 @@ void Runge_Kutta::moveParticles(const double endTime)
 			_mm_store_pd(cloud->l3 + i, vdt*cloud->getVx3_pd(i)); // positionX tidbit
 			_mm_store_pd(cloud->m3 + i, vdt*_mm_load_pd(pFy)/vmass); // velocityY tidbit
 			_mm_store_pd(cloud->n3 + i, vdt*cloud->getVy3_pd(i)); // positionY tidbit
-			_mm_store_pd(cloud->q3 + i, -vdt*(qConst1*cloud->getq2_pd(i) +
-				qConst2*partRadius*_mm_load_pd(cloud->phi + i)));
+			//_mm_store_pd(cloud->q3 + i, -vdt*(qConst1*cloud->getq2_pd(i) +
+			//	qConst2*partRadius*_mm_load_pd(cloud->phi + i)));
+			_mm_store_pd(cloud->q3 + i, _mm_load_pd(cloud->charge + i));
 
 			// reset forces to zero:
 			_mm_store_pd(pFx, _mm_setzero_pd());
@@ -162,8 +165,9 @@ void Runge_Kutta::moveParticles(const double endTime)
 			_mm_store_pd(cloud->l4 + i, vdt*cloud->getVx4_pd(i)); // positionX tidbit
 			_mm_store_pd(cloud->m4 + i, vdt*_mm_load_pd(pFy)/vmass); // velocityY tidbit
 			_mm_store_pd(cloud->n4 + i, vdt*cloud->getVy4_pd(i)); // positionY tidbit
-			_mm_store_pd(cloud->q4 + i, -vdt*(qConst1*cloud->getq3_pd(i) +
-				qConst2*partRadius*_mm_load_pd(cloud->phi + i)));
+			//_mm_store_pd(cloud->q4 + i, -vdt*(qConst1*cloud->getq3_pd(i) +
+			//	qConst2*partRadius*_mm_load_pd(cloud->phi + i)));
+			_mm_store_pd(cloud->q4 + i, _mm_load_pd(cloud->charge + i));
 
 			// reset forces to zero:
 			_mm_store_pd(pFx, _mm_setzero_pd());
@@ -217,7 +221,7 @@ void Runge_Kutta::moveParticles(const double endTime)
 			_mm_store_pd(px, _mm_load_pd(px) + (vl1 + v2*(vl2 + vl3) + vl4)/v6);
 			_mm_store_pd(pVy, _mm_load_pd(pVy) + (vm1 + v2*(vm2 + vm3) + vm4)/v6);
 			_mm_store_pd(py, _mm_load_pd(py) + (vn1 + v2*(vn2 + vn3) + vn4)/v6);
-			_mm_store_pd(pQ, _mm_load_pd(pQ) + (vq1 + v2*(vq2 + vq3) + vq4)/v6);
+			_mm_store_pd(pQ, (vq1 + v2*(vq2 + vq3) + vq4)/v6);
 		}
 
 		currentTime += dt;
